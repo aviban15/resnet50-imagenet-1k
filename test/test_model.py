8a5f8d8d23c3bb11f309b94338e80e923e8c91fc
Namrata Nerurkar <namratanerurkar02@gmail.com>
2025-10-21 03:21:58 +0530

    test_model.py's imports updated

--- test/test_model.py
+++ test/test_model.py
@@ -6,19 +6,19 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Import torch packages
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import datasets, transforms
-from torchsummary import summary
-import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torchvision import datasets, transforms
+# from torchsummary import summary
+# import torch.optim as optim
+# from torch.optim.lr_scheduler import StepLR
 
 # Other packages
-from tqdm import tqdm
+# from tqdm import tqdm
 
 # Import model
-from src.model import get_resnet50
+from src.model import get_resnet50 # absolute import
 
 ## Test Model ##
 
